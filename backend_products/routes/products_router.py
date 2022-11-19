497e328cc4291a21f919630457529af6da25ffe6
Krzaq8 <kubakropkazet@gmail.com>
2022-11-19 20:41:37 +0100

    now connects to firebase

--- backend_products/routes/products_router.py
+++ backend_products/routes/products_router.py
@@ -1,5 +1,5 @@
 start_products = ['tomato', 'cheese', 'pasta']
-import json
+# import json
 import firebase_admin as fa
 from firebase_admin import db
 
@@ -9,24 +9,49 @@ class ProductRouter():
     self.products = dict()
     cred_obj = fa.credentials.Certificate('./credentials/fridgefront-a501e-firebase-adminsdk-xvue4-b225b21e7b.json')
     fa.initialize_app(cred_obj, {'databaseURL': "https://fridgefront-a501e-default-rtdb.firebaseio.com"})
-    self.db = db.reference('/')
 
-  def ensure_user_exists(self, user):
-    if self.products.get(user) is None:
-      self.db.set(json.dumps(dict({user: start_products})))
-      self.products[user] = start_products
+  # returns any reference for user
+  def get_ref_for(self, user):
+    return db.reference('/Users/{}'.format(user))
+
+  # returns validated reference for user
+  def get_user(self, user):
+    ref = self.get_ref_for(user)
+    if ref.get() is None:
+      ref.set(start_products)
+    return ref
 
   def list(self, user):
-    self.ensure_user_exists(user)
-    # return self.products[user]
-    return self.db.get(user)
+    ref = self.get_user(user)
+    return self.get_ref_for(user).get()
 
   def add(self, user, new_products):
-    self.ensure_user_exists(user)
-    self.products[user] = self.products[user] | set(new_products)
+    ref = self.get_user(user)
+    products = list(set(ref.get()) | set(new_products))
+    ref.set(products)
+    
 
   def remove(self, user, removed_products):
-    self.ensure_user_exists(user)
-    self.products[user] = self.products[user] - set(removed_products)
+    ref = self.get_user(user)
+    products = list(set(ref.get()) - set(removed_products))
+    ref.set(products)
 
 
+
+# cred_obj = fa.credentials.Certificate('./credentials/fridgefront-a501e-firebase-adminsdk-xvue4-b225b21e7b.json')
+# fa.initialize_app(cred_obj, {'databaseURL': "https://fridgefront-a501e-default-rtdb.firebaseio.com"})
+
+
+# ref = db.reference("/")
+# ref.set({
+# 	"Users":
+# 	{
+# 		"hifhiu3h8-34n3ihb-di3hd": [
+#       'tomato',
+#       'cheese'
+#     ]
+# 	}
+# })
+
+# ref = db.reference("/Users/hifhiu3h8-34n3ihb-di3hd")
+# ref.set(['tomato'])
